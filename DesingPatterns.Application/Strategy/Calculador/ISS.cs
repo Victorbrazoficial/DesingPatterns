ddec07511272196260a280c2d391c31cdf5b7094
VictorBrazOficial <vic_braz@outlook.com>
2023-04-01 09:31:57 -0300

    Criando novo Imposto

---
+++ DesingPatterns.Application/Strategy/Calculador/ISS.cs
@@ -0,0 +1,33 @@
+﻿using DesingPatterns.Application.Strategy.Calculador.Interface;
+using DesingPatterns.Application.Strategy.Model.Imposto.ICMS;
+using DesingPatterns.Application.Strategy.Repositories;
+using DesingPatterns.Domain.Strategy.Imposto;
+
+namespace DesingPatterns.Application.Strategy.Calculador
+{
+    public class ISS : IImposto
+    {
+        private readonly ITaxaImpostoRepository _taxaImpostoRepository;
+
+        public ISS(ITaxaImpostoRepository taxaImpostoRepository)
+        {
+            _taxaImpostoRepository = taxaImpostoRepository;
+        }
+
+        public async Task<CalculadorDeImposto> Calcular(ObterICMSRequest request)
+        {
+            var iss = await _taxaImpostoRepository.GetISS();
+
+            var resultado = request.Valor - (request.Valor * iss.TaxaImposto / 100);
+
+            var response = new CalculadorDeImposto()
+            {
+                NomeImposto = request.imposto,
+                TaxaImposto = iss.TaxaImposto,
+                ValorComImpostoDeduzido = resultado
+            };
+
+            return response;
+        }
+    }
+}
---
+++ DesingPatterns.Application/Strategy/Calculador/Interface/IImposto.cs
@@ -0,0 +1,10 @@
+﻿using DesingPatterns.Application.Strategy.Model.Imposto.ICMS;
+using DesingPatterns.Domain.Strategy.Imposto;
+
+namespace DesingPatterns.Application.Strategy.Calculador.Interface
+{
+    public interface IImposto
+    {
+        Task<CalculadorDeImposto> Calcular(ObterICMSRequest request);
+    }
+}
