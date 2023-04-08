464cac49cf6604774a12f94ef4eb2e32fa3915a2
VictorBrazOficial <vic_braz@outlook.com>
2023-04-08 17:48:58 -0300

    Adicionando Repository padrão Chain of Reponsability

---
+++ DesingPatterns.Application/ChainOfResponsability/Repositories/ITaxaDescontoRepository.cs
@@ -0,0 +1,10 @@
+﻿using DesingPatterns.Application.ChainOfResponsability.Model;
+using DesingPatterns.Domain.ChainOfResponsability.Desconto;
+
+namespace DesingPatterns.Application.ChainOfResponsability.Repositories
+{
+    public interface ITaxaDescontoRepository
+    {
+        Task<DescontoEntity> GetDescontoPorCincoItens(DescontoRequest request);
+    }
+}
---
+++ DesingPatterns.Infrastructure/Repositories/ChainOfResponsability/TaxaDescontoRepository.cs
@@ -0,0 +1,40 @@
+﻿using DesingPatterns.Application.ChainOfResponsability.Model;
+using DesingPatterns.Application.ChainOfResponsability.Repositories;
+using DesingPatterns.Domain.ChainOfResponsability.Desconto;
+using Item = DesingPatterns.Domain.ChainOfResponsability.Desconto.Item;
+
+namespace DesingPatterns.Infrastructure.Repositories.ChainOfResponsability
+{
+    public class TaxaDescontoRepository : ITaxaDescontoRepository
+    {
+        private const decimal TaxaDescontoPorCincoItens = 0.1m;
+        public async Task<DescontoEntity> GetDescontoPorCincoItens(DescontoRequest request)
+        {
+            var descontoEntity = AddItem(request);
+
+            descontoEntity.TaxaDescotno = TaxaDescontoPorCincoItens;
+
+            return descontoEntity;
+        }
+
+        private DescontoEntity AddItem(DescontoRequest request)
+        {
+            var descontoEntity = new DescontoEntity()
+            {
+                Itens = new List<Item>()
+            };
+
+            foreach (var item in request.Itens)
+            {
+                descontoEntity.Itens.Add(
+                    new Item() 
+                    { 
+                        Nome = item.NomeProduto,
+                        Valor = item.ValorProduto 
+                    });
+            }
+
+            return descontoEntity;
+        }
+    }
+}
