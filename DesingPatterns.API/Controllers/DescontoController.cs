c78010348edf4898a79e7f372047c0115a7d7759
VictorBrazOficial <vic_braz@outlook.com>
2023-04-08 17:52:10 -0300

    adicionando controller padrão Chain of Reponsability

---
+++ DesingPatterns.API/Controllers/DescontoController.cs
@@ -0,0 +1,20 @@
+﻿using DesingPatterns.Application.ChainOfResponsability.Model;
+using DesingPatterns.Application.ChainOfResponsability.UseCase.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesingPatterns.API.Controllers
+{
+    [Route("api/desconto/")]
+    [ApiController]
+    public class DescontoController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetDesconto([FromBody] DescontoRequest request, [FromServices] IDescontoUseCase useCase)
+        {
+            var response = await useCase.Execute(request);
+
+            return Ok(response);
+        }
+
+    }
+}
--- DesingPatterns.API/Program.cs
+++ DesingPatterns.API/Program.cs
@@ -1,8 +1,14 @@
+using DesingPatterns.Application.ChainOfResponsability.Calculador;
+using DesingPatterns.Application.ChainOfResponsability.Calculador.Interface;
+using DesingPatterns.Application.ChainOfResponsability.Repositories;
+using DesingPatterns.Application.ChainOfResponsability.UseCase;
+using DesingPatterns.Application.ChainOfResponsability.UseCase.Interface;
 using DesingPatterns.Application.Strategy.Calculador;
 using DesingPatterns.Application.Strategy.Calculador.Interface;
 using DesingPatterns.Application.Strategy.Repositories;
 using DesingPatterns.Application.Strategy.UseCase.Imposto;
 using DesingPatterns.Application.Strategy.UseCase.Imposto.Interface;
+using DesingPatterns.Infrastructure.Repositories.ChainOfResponsability;
 using DesingPatterns.Infrastructure.Strategy.Repositories.Imposto;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +24,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IObterImpostoDeduzidoUseCase, ObterImpostoDeduzidoUseCase>();
 builder.Services.AddScoped<ITaxaImpostoRepository, TaxaImpostoRepository>();
 builder.Services.AddScoped<ICalculador, Calculador>();
+builder.Services.AddSingleton<IDescontoUseCase, DescontoUseCase>();
+builder.Services.AddSingleton<ITaxaDescontoRepository, TaxaDescontoRepository>();
+builder.Services.AddSingleton<ICalculadorDescontos, CalculadorDescontos>();
 
 
 var app = builder.Build();
