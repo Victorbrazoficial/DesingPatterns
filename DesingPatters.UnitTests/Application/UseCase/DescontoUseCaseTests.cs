5d11f81687c304cb2e4e1623648dc92c91d2e1a2
VictorBrazOficial <vic_braz@outlook.com>
2023-04-15 10:17:22 -0300

    TesteUnitarios Descontos

---
+++ DesingPatters.UnitTests/Application/UseCase/DescontoUseCaseTests.cs
@@ -0,0 +1,113 @@
+﻿using DesingPatterns.Application.ChainOfResponsability.Calculador.Interface;
+using DesingPatterns.Application.ChainOfResponsability.Model;
+using DesingPatterns.Application.ChainOfResponsability.UseCase;
+using DesingPatters.UnitTests.Mock;
+using FluentAssertions;
+using Moq;
+
+namespace DesingPatters.UnitTests.Application.UseCase
+{
+    public class DescontoUseCaseTests
+    {
+        private readonly Mock<ICalculadorDescontos> _calculador;
+        private readonly DescontoUseCase _useCase;
+
+        public DescontoUseCaseTests()
+        {
+            _calculador = new Mock<ICalculadorDescontos>();
+            _useCase = new DescontoUseCase(_calculador.Object);
+        }
+
+        [Fact]
+        public async void ExecuteComDesconto_Sucesso()
+        {
+            var request = new DescontoRequest()
+            {
+                Itens = new List<Item>()
+                {
+                    new Item()
+                    {
+                        NomeProduto = "nome produto1",
+                        ValorProduto = 5.50m
+                    },
+                    new Item()
+                    {
+                        NomeProduto = "nome produto2",
+                        ValorProduto = 5.50m
+                    },
+                    new Item()
+                    {
+                        NomeProduto = "nome produto3",
+                        ValorProduto = 5.50m
+                    },
+                    new Item()
+                    {
+                        NomeProduto = "nome produto4",
+                        ValorProduto = 5.50m
+                    },
+                    new Item()
+                    {
+                        NomeProduto = "nome produto5",
+                        ValorProduto = 5.50m
+                    },
+                    new Item()
+                    {
+                        NomeProduto = "nome produto6",
+                        ValorProduto = 5.50m
+                    }
+                }
+            };
+
+            var response = DescontoMock.DescontoResponse(request);
+
+            var teste = _calculador.Setup(x => x.Desconta(request)).Returns(response);
+
+            var result = await _useCase.Execute(request);
+
+            result.Should().NotBeNull();
+            result.Texto = "Você atingiu mais de cinco itens por isso recebeu um desconto de 0.1% em relção ao valor total.";
+            result.ValorDesconto.Should().Be(3.30m);
+        }
+
+        [Fact]
+        public async void ExecuteSemDesconto_Sucesso()
+        {
+            var request = new DescontoRequest()
+            {
+                Itens = new List<Item>()
+                {
+                    new Item()
+                    {
+                        NomeProduto = "nome produto1",
+                        ValorProduto = 5.50m
+                    },
+                    new Item()
+                    {
+                        NomeProduto = "nome produto2",
+                        ValorProduto = 5.50m
+                    },
+                    new Item()
+                    {
+                        NomeProduto = "nome produto3",
+                        ValorProduto = 5.50m
+                    },
+                    new Item()
+                    {
+                        NomeProduto = "nome produto4",
+                        ValorProduto = 5.50m
+                    }
+                }
+            };
+
+            var response = DescontoMock.SemDescontoResponse();
+
+            var teste = _calculador.Setup(x => x.Desconta(request)).Returns(response);
+
+            var result = await _useCase.Execute(request);
+
+            result.Should().NotBeNull();
+            result.Texto = "Se você atingir a quantidade de seis itens você recebera um desconto.";
+            result.ValorDesconto.Should().Be(0.0m);
+        }
+    }
+}
---
+++ DesingPatters.UnitTests/Mock/DescontoMock.cs
@@ -0,0 +1,46 @@
+﻿using DesingPatterns.Application.ChainOfResponsability.Model;
+using DesingPatterns.Domain.ChainOfResponsability.Desconto;
+using Item = DesingPatterns.Domain.ChainOfResponsability.Desconto.Item;
+
+namespace DesingPatters.UnitTests.Mock
+{
+    public static class DescontoMock
+    {
+        public static async Task<DescontoEntity> DescontoResponse(DescontoRequest request)
+        {
+            const decimal taxaDesconto = 0.1m;
+
+            var response = new DescontoEntity()
+            {
+                TaxaDescotno = taxaDesconto,
+                Texto = "Você atingiu mais de cinco itens por isso recebeu um desconto de 0.1% em relção ao valor total.",
+                Valor = 0.0m,               
+            };
+
+            response.Valor = CalculaDesconto(response, request);
+
+            return response;
+        }
+
+        public static async Task<DescontoEntity> SemDescontoResponse()
+        {
+            var response = new DescontoEntity()
+            {
+                Texto = "Se você atingir a quantidade de seis itens você recebera um desconto.",
+                Valor = 0.0m,
+            };
+
+            return response;
+        }
+
+        private static decimal CalculaDesconto(DescontoEntity descontoEntity, DescontoRequest request)
+        {
+            foreach (var item in request.Itens)
+            {
+                descontoEntity.Valor += item.ValorProduto;
+            }
+            var calculoTaxa = descontoEntity.Valor * descontoEntity.TaxaDescotno;
+            return calculoTaxa;
+        }
+    }
+}
