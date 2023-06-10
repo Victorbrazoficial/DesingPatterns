dcc2a61a3e2e22904b5f4a2ce0ffab07317102fb
VictorBrazOficial <vic_braz@outlook.com>
2023-06-10 14:08:56 -0300

    Decorator Infra

---
+++ DesingPatterns.Infrastructure/Decorator/Repositories/DecoratorReository.cs
@@ -0,0 +1,48 @@
+﻿using DesingPatterns.Application.Decorator.Model.Imposto;
+using DesingPatterns.Application.Decorator.Repositories;
+using DesingPatterns.Domain.Decorator.Constante;
+using DesingPatterns.Domain.Decorator.Imposto;
+
+namespace DesingPatterns.Infrastructure.Decorator.Repositories
+{
+    public class DecoratorReository : IDecoratorRepository
+    {
+        private const decimal TaxaICMS = 12M;
+        private const decimal TaxaISS = 2M;
+
+        public List<ImpostoDecoratorEntity> GetTaxaImposto(DecoratorRequest request)
+        {
+            var listaEntity = new List<ImpostoDecoratorEntity>();
+
+            
+            foreach (var imposto in request.Imposto)
+            {
+                if (imposto.Equals(ImpostosDecoratorConstantes.ISS))
+                {
+                    var entity = new ImpostoDecoratorEntity()
+                    {
+                        NomeImposto = ImpostosDecoratorConstantes.ISS,
+                        TaxaImposto = TaxaISS
+
+                    };
+                    listaEntity.Add(entity);
+                } 
+                else if (imposto.Equals(ImpostosDecoratorConstantes.ICMS))
+                {
+                    var entity = new ImpostoDecoratorEntity()
+                    {
+                        NomeImposto = ImpostosDecoratorConstantes.ICMS,
+                        TaxaImposto = TaxaICMS
+                    };
+                    listaEntity.Add(entity);
+                }
+                else
+                {
+                    throw new Exception($"o imposto [{imposto}] não existe");
+                }
+            }
+
+            return listaEntity;
+        }
+    }
+}
